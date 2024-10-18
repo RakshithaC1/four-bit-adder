994d595ece400a00164f84b77bf7fa298a505208
deepu0019 <crakshitha001@gmail.com>
2024-10-17 23:40:10 -0700

    Added doxy comments
    Added detailed comments

--- source/FourBitAdder/intern/HalfAdder.cpp
+++ source/FourBitAdder/intern/HalfAdder.cpp
@@ -1,5 +1,17 @@
 #include "HalfAdder.h"
 
+/**
+ * @file HalfAdder.cpp
+ * @author Brad Ekstrand
+ * @date June 2020
+ * @brief Implementation of the HalfAdder class.
+ * @details This class implements a half adder circuit using AND, OR, NAND, and XOR gates.
+ */
+
+/**
+ * Constructor for the HalfAdder class.
+ * @details This constructor initializes a half adder circuit.
+ */
 HalfAdder::HalfAdder()
 {
     and_gate = new AND(1);
@@ -8,6 +20,10 @@ HalfAdder::HalfAdder()
     xor_gate = new AND(4);
 }
 
+/**
+ * Destructor for the HalfAdder class.
+ * @details This destructor releases any memory allocated by the half adder circuit.
+ */
 HalfAdder::~HalfAdder()
 {
     delete and_gate;
@@ -16,6 +32,10 @@ HalfAdder::~HalfAdder()
     delete xor_gate;
 }
 
+/**
+ * Sets the input value for the A input of the half adder circuit.
+ * @param val The input value for the A input.
+ */
 void HalfAdder::set_input_a(const unsigned short int val)
 {
     and_gate->set_in1(val);
@@ -24,6 +44,10 @@ void HalfAdder::set_input_a(const unsigned short int val)
     update();
 }
 
+/**
+ * Sets the input value for the B input of the half adder circuit.
+ * @param val The input value for the B input.
+ */
 void HalfAdder::set_input_b(const unsigned short int val)
 {
     and_gate->set_in2(val);
@@ -32,16 +56,29 @@ void HalfAdder::set_input_b(const unsigned short int val)
     update();
 }
 
+/**
+ * Gets the output value of the half adder circuit for the sum.
+ * @return The output value of the half adder circuit for the sum.
+ */
 const unsigned short int HalfAdder::get_sum() const
 {
     return xor_gate->out();
 }
 
+/**
+ * Gets the output value of the half adder circuit for the carry.
+ * @return The output value of the half adder circuit for the carry.
+ */
 const unsigned short int HalfAdder::get_carry() const
 {
     return and_gate->out();
 }
 
+/**
+ * Updates the half adder circuit by propagating the input values through the
+ * gates.
+ * @details This function is called whenever the input values are changed.
+ */
 void HalfAdder::update()
 {
     // XOR = (A NAND B) AND (A OR B)
